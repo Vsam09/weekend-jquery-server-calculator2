688d4d77c9cf1906a0084d9c9a14481358a65b99
samson vang <vangsam2@gmail.com>
2021-07-31 17:59:43 -0500

    Stuck with operators not calculating

--- server/public/client.js
+++ server/public/client.js
@@ -1,9 +1,32 @@
 console.log('Client JS')
 
+let results = [{}];
+
 $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery')
     $('#clear').on('click', clearInputs)
+    $('#add').on('click', add)
+}
+
+function clearInputs() {
+    console.log('Clear Inputs')
+    $('#firstInput').val(),
+    $('#secondInput').val()  
+};
+function add() {
+    let input = inputs();
+    results.push(input);
+    console.log('results', results)
+}
+function inputs() {
+    console.log('firstInput')
+    let input = { 
+        firstInput: Number ($('#firstInput').val()),
+        secondInput: Number ($('#secondInput').val())
+        
+    };
+    return input;
 }
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+let result = [{}];
+
 const app = express();
 const bodyParser = require('body-parser');
 const port = 5000;
@@ -8,6 +10,7 @@ app.use(express.static('server/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.listen(port, () => {
     console.log('listening on port', port)
