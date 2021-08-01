86d88e381c442d18dc1f73abae8d5358069f4129
samson vang <vangsam2@gmail.com>
2021-08-01 17:41:38 -0500

    stuck on how to get operators to work

--- server/public/client.js
+++ server/public/client.js
@@ -30,3 +30,28 @@ function inputs() {
     return input;
 }
 
+// take the operator input
+const operator = operator;
+
+// take the operand input
+const number1 = $('#firstInput');
+const number2 = $('#secondInput');;
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
