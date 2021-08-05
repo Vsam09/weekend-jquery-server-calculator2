6ada6afbdbc6d97d20daf3b934bc3023cfc702b1
samson vang <vangsam2@gmail.com>
2021-08-04 20:46:41 -0500

    ajax added to equals function to post inputs

--- server/public/client.js
+++ server/public/client.js
@@ -1,3 +1,5 @@
+const { urlencoded } = require("body-parser");
+
 console.log('Client JS')
 
 let operator = '';
@@ -11,16 +13,23 @@ function onReady() {
     $('#equals').on('click', equals)
 }
 
+//Create C button and clear inputs
+//when C button is clicked
 function clearInputs() {
     console.log('Clear Inputs')
     $('#firstInput').val(''),
     $('#secondInput').val('')  
 };
+//Operator button should work "on click for all buttons"
 function operatorButtons() {
-    console.log('operator is working');
+    console.log('operators is working');
     operator = $(this).text();
-}
+};
 
+//Create function for operator " = " button
+//It should also calculate the 2 inputs with
+//Selected operators add, subtract, divide, or multiple
+//!! Don't forget about $.ajax(method, url) !!
 function equals() {
     console.log('equals works')
     let input = { 
@@ -28,5 +37,13 @@ function equals() {
         secondInput: Number ($('#secondInput').val()),
         operator: operator
     };
-    return input;
-}
+    console.log('inputs', input);
+    $.ajax ({
+        method: 'POST',
+        url: '/result',
+        data: input
+    })
+    .then( function(response) {
+        console.log(response)
+    })
+};
